536772a3e58c311b433f58fcacb65024cac531cf
Bill Brinkert <william.brinkert@akqa.com>
2020-02-04 11:01:15 -0800

    docs: update jsdoc style comments

--- pages/api/FaunaConnection.js
+++ pages/api/FaunaConnection.js
@@ -36,8 +36,8 @@ class FaunaConnection {
       this.q.CreateIndex({
         name: name,
         source: this.q.Collection(src),
-        terms: [terms],
-        values: [values]
+        terms: terms,
+        values: values
       })
     )
   }
@@ -81,6 +81,13 @@ class FaunaConnection {
     )
   }
 
+  /**
+   * Retrieve a document given its ref and collection
+   * 
+   * @param  {string} ref        the ID or ref of the document
+   * @param  {string} collection the name of the collection
+   * @return {Object}            the document identified by the query
+   */
   get(ref, collection) {
     return this.client.query(
       this.q.Get(
@@ -92,6 +99,17 @@ class FaunaConnection {
     )
   }
 
+  /**
+   * Retrieve the first document that matches the given value 
+   * of a term used to create the index. For example, if we 
+   * have an index called "posts_by_title" whose term was set 
+   * to "title", we can search that index using a value of 
+   * "My Blog Post" to see if any posts' titles match our value.
+   * 
+   * @param  {string} index name of index to search on
+   * @param  {multiple} value the value to match on the index term
+   * @return {Object}       the first matching document
+   */
   getMatch(index, value) {
     return this.client.query(
       this.q.Get(
@@ -103,7 +121,15 @@ class FaunaConnection {
     )
   }
 
-  getAllRefs(index, size) {
+  /**
+   * Given an index 'all_<items>' with no set terms or values,
+   * this will return the refs of all documents in that index.
+   * 
+   * @param  {[type]} index [description]
+   * @param  {[type]} size  [description]
+   * @return {[type]}       [description]
+   */
+  getAllRefsByIndex(index, size) {
     return this.client.query(
       this.q.Paginate(
         this.q.Match(
@@ -118,23 +144,37 @@ class FaunaConnection {
     return this.q.Ref(this.q.Collection(options.collection), options.ref)
   }
 
-  getAllByIndex(index, size, before, after) {
-    if (before.collection) {
-      before = this._buildRef(before)
+  /**
+   * Provided with an index, returns all matching documents
+   * 
+   * @param  {string} index   name of the index
+   * @param  {Object} options options that may be passed in include:
+   * before - ref to document for traversing pagination backward
+   * after - ref to document for traversing pagination forward
+   * term - a such term to use with a searchable index
+   * size - the number of docs to return per page
+   * scope - the database ref within which to perform the match
+   * @return {Object}         Returns a collection of docs matching 
+   * the query conditions
+   */
+  getAllDocsByIndex(index, options = {}) {
+    if (options.before && options.before.collection) {
+      options.before = this._buildRef(options.before)
     } 
-    else if (after.collection) {
-      after = this._buildRef(after)
+    else if (options.after && options.after.collection) {
+      options.after = this._buildRef(options.after)
     }
     return this.client.query(
       this.q.Map(
         this.q.Paginate(
           this.q.Match(
-            this.q.Index(index)
+            this.q.Index(index, options.scope),
+            options.term
           ),
           { 
-            size: size || this.size,
-            before: before || undefined,
-            after: after || undefined
+            size: options.size || this.size,
+            before: options.before,
+            after: options.after
            }
         ),
         ref => this.q.Get(ref) 
@@ -142,6 +182,15 @@ class FaunaConnection {
     )
   }
 
+  /**
+   * Given the ref and collection this will update the
+   * data of that document.
+   * 
+   * @param  {string} ref        the ID or ref of the doc
+   * @param  {string} collection the name of the collection
+   * @param  {Object} data       the data to be updated
+   * @return {Object}            the updated document
+   */
   update(ref, collection, data) {
     return this.client.query(
       this.q.Update(
@@ -154,6 +203,17 @@ class FaunaConnection {
     )
   }
 
+  /**
+   * Given the ref and collection this will replace the
+   * data of that document whose fields match the given data.
+   * Any old fields in the document that are not mentioned in
+   * the data param are removed.
+   * 
+   * @param  {string} ref        the ID or ref of the doc
+   * @param  {string} collection the name of the collection
+   * @param  {Object} data       the data to be replace
+   * @return {Object}            the updated document
+   */
   replace(ref, collection, data) {
     return this.client.query(
       this.q.Replace(
@@ -166,6 +226,12 @@ class FaunaConnection {
     )
   }
 
+  /**
+   * Destroys a document
+   * @param  {string} ref        the ID or ref of the doc
+   * @param  {string} collection the name of the collection
+   * @return {Object}            the deleted document
+   */
   delete(ref, collection) {
     return this.client.query(
       this.q.Delete(
@@ -199,14 +265,15 @@ class FaunaConnection {
 
 const fauna = new FaunaConnection({secret: secret})
 
+opts = {
+  term: 'Alan'
+}
+
 fauna
-  .getAllByIndex('all_letters', 3)
+  .getAllRefsByIndex('posts_by_title')
   .then(res => {
     console.log(res)
-    // fauna
-    //   .getAllByIndex('all_letters', 3, 0, res.after)
-    //   .then(res => console.log(res))
-    //   .catch(err => console.log(err))
+    if (res.data.length > 0) res.data.forEach(doc => console.log('doc: ', doc))
 
   })
   .catch(err => console.log(err))
